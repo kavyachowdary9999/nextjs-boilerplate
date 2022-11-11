58f4c312753029f80aad6805eb3521f2e8d691ca
kavyachowdary9999 <37150638+kavyachowdary9999@users.noreply.github.com>
2022-11-10 22:43:41 -0500

    added chess game

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,9 @@ export default function Home() {
           Page owned by knannap@clemson.edu
         </p>
 
+<p className={styles.description}>
+          <a href="chess" className={styles.card}>Play chess</a>
+        </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
